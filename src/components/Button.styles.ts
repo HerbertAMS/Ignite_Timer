2cbde1600962afaade956b34e28ff89057ae2643
HerbertAMS <herbert.ams@gmail.com>
2023-02-13 16:46:00 -0300

    extends styled-components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,23 +1,26 @@
-import styled from 'styled-components'
+import styled, { css } from "styled-components";
 
-export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
 
 interface ButtonContainerProps {
-    variant: ButtonVariant;
+  variant: ButtonVariant;
 }
 
 const buttonVariants = {
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
-}
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
 
-export const ButtonContainer = styled.button<ButtonContainerProps> `
-    width: 100px;
-    height: 40px;
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
 
-    ${props => {
-        return `background-color: ${buttonVariants[props.variant]}`
-    }}
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
 `
